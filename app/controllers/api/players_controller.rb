74962395124828dd29fcc49209ad9c0175a677a4
Gabe Dalay <gabedalay@gmail.com>
2015-01-09 21:29:09 -0600

    Added create player endpoit

--- app/controllers/api/players_controller.rb
+++ app/controllers/api/players_controller.rb
@@ -3,4 +3,24 @@ class Api::PlayersController < ApplicationController
     players = Player.all
     render json: players, status: 200
   end
+
+  def create
+    player = Player.new(player_params)
+    if player.save
+      render json: player, status: :created #, location: player
+    else
+      render json: player.errors, status: 402
+    end
+  end
+
+  private
+    def player_params
+      params.require(:player).permit(:facebookid,
+                                    :name,
+                                    :gender,
+                                    :age,
+                                    :location,
+                                    :likes_men,
+                                    :likes_women)
+    end
 end
--- test/integration/player_resources_test.rb
+++ test/integration/player_resources_test.rb
@@ -8,4 +8,24 @@ class PlayerResourcesTest < ActionDispatch::IntegrationTest
     assert_equal 200, response.status
     refute_empty response.body
   end
+
+  test "should create a new player" do
+    post "/players", { player: { facebookid: "1234567890",
+                                      name: "Gregor Reptile",
+                                      first_name: "Gregor",
+                                      last_name: "Reptile",
+                                      gender: :male,
+                                      age: "36",
+                                      location: "Saint Louis",
+                                      likes_men: 0,
+                                      likes_women: 1 }
+                                }.to_json,
+                                { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
+
+    assert_equal 201, response.status
+    assert_equal Mime::JSON, response.content_type
+  end
+
+  # test "should return a list of players based on a specific player's preference" do
+  #   get "/pla"
 end
